7d4bf29448b4c91b34f461bb5df92015286e2ea7
HariyaNobuki <HariyaNobuki@users.noreply.github.com>
2022-08-31 08:27:33 +0900

    きついめう

--- makemask.py
+++ makemask.py
@@ -1,7 +1,9 @@
+#from readline import append_history_file
 import cv2
 import numpy as np
 import pandas as pd
 import os
+from skimage import io, color
 
 def makecirkle(X,Y):
     cv2.circle(img,
@@ -19,6 +21,62 @@ def makecirkle_Y(X,Y):
         thickness=3,
         lineType=cv2.LINE_4,
         shift=0)
+
+def con2Lab(B,G,R):
+    L = []
+    a = []
+    b = []
+    for i in range(len(B)):
+        if R[i]/255 <= 0.040450:
+            R_ = R[i]/255/12.92
+        else:
+            R_ = ((R[i]/255+0.055)/1.055)**(2.4)
+        if G[i]/255 <= 0.040450:
+            G_ = G[i]/255/12.92
+        else:
+            G_ = ((G[i]/255+0.055)/1.055)**(2.4)
+        if B[i]/255 <= 0.040450:
+            B_ = B[i]/255/12.92
+        else:
+            B_ = ((B[i]/255+0.055)/1.055)**(2.4)
+
+        X=0.4124*R_+0.3576*G_+0.1805*B_
+        Y=0.2126*R_+0.7152*G_+0.0722*B_
+        Z=0.0193*R_+0.1192*G_+0.9505*B_
+
+        Xn = 0.9504
+        Yn = 1
+        Zn = 1.0889
+
+        if X/Xn > (6/29)**3:
+            fX = (X/Xn)**(1/3)
+        else:
+            fX = (1/3)*((29/6)**2)*(X/Xn)+(4/29)
+        if Y/Yn > (6/29)**3:
+            fY = (Y/Yn)**(1/3)
+        else:
+            fY = (1/3)*((29/6)**2)*(Y/Yn)+(4/29)
+        if Z/Zn > (6/29)**3:
+            fZ = (Z/Zn)**(1/3)
+        else:
+            fZ = (1/3)*((29/6)**2)*(Z/Zn)+(4/29)
+        
+        L_i = 116*fY - 16
+        a_i = 500*(fX-fY)
+        b_i = 200*(fY-fZ)
+
+        L.append(L_i)
+        a.append(a_i)
+        b.append(b_i)
+    df_Lab = pd.DataFrame({
+        "L" : L,
+        "a" : a,
+        "b" : b,
+    })
+    return df_Lab
+
+
+
 
 
 def conv2RGB(img):
@@ -203,6 +261,7 @@ def conv2RGB(img):
         boxToX = point[0]+2 #対象範囲終了位置 X座標
         boxToY = point[1]+2 #対象範囲終了位置 Y座標
         # y:y+h, x:x+w　の順で設定
+        # 青、緑、赤
         imgBox = img[boxFromY:boxToY, boxFromX:boxToX]
         b = imgBox.T[0].flatten().mean()
         g = imgBox.T[1].flatten().mean()
@@ -216,10 +275,14 @@ def conv2RGB(img):
         R.append(int(r))
         G.append(int(g))
         B.append(int(b))
+    df_Lab = con2Lab(B,G,R)
     df = pd.DataFrame({
         "R" : R,
         "G" : G,
         "B" : B,
+        "L" : df_Lab["L"],
+        "a" : df_Lab["a"],
+        "b" : df_Lab["b"],
     })
     df.to_csv(savepath + "/point_rbf.csv")
 
@@ -237,3 +300,25 @@ for i in gamma1_param:
     savepath = gamma1_path + "/" + str(i)
     conv2RGB(img)
 print("###FIN GAMMA1###")
+
+
+gamma2_path = init_path + "/gamma2"
+gamma2_param = [0.5 , 0.7 , 0.8 , 0.9 , 1.0 , 1.1 , 1.2 ,1.3]
+for i in gamma2_param:
+    img_path = gamma2_path + "/" + str(i)
+    img = cv2.imread(img_path+'/output.bmp')
+    savepath = gamma2_path + "/" + str(i)
+    conv2RGB(img)
+print("###FIN GAMMA2###")
+
+
+gamma3_path = init_path + "/gamma3"
+gamma3_param1 = [0.2 , 0.3 , 0.4 , 0.5 , 0.6 , 0.7 , 0.8 , 0.9]
+gamma3_param2 = [0.224 , 0.336 , 0.448 , 0.56 , 0.672 , 0.784 , 0.896 ,1.008]
+count = 0
+for i in gamma3_param1:
+    img_path = gamma3_path + "/" + str(i)
+    img = cv2.imread(img_path+'/output.bmp')
+    savepath = gamma3_path + "/" + str(i)
+    conv2RGB(img)
+print("###FIN GAMMA3###")
